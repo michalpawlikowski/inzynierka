2f1a28ff86a5464f17d48616c211974fbbdd56f5
michalpawlikowski <73520426+michalpawlikowski@users.noreply.github.com>
2023-01-24 00:04:32 +0100

    asdasfasf

--- dobrylekarz/app/Http/Controllers/SearchController.php
+++ dobrylekarz/app/Http/Controllers/SearchController.php
@@ -13,7 +13,10 @@ class SearchController extends Controller
     public function index(Request $request)
     {
         $specializations = $request->specializations;
-        $city = $request->cities;
+        $city = $request->city;
+
+
+        $userId = Auth::id();
 
         
             $listoffer = DB::table('offer')
@@ -22,7 +25,7 @@ class SearchController extends Controller
             ->join('offeraddres', 'offer.id', '=', 'offer_id')
             ->join('miasta', 'offeraddres.miasto_id', '=', 'miasta.id')
             ->join('woje', 'woje.id', '=', 'miasta.woj_id')
-            ->select('offer.*', 'specializations.name as specializations', 'users.name as name1', 'users.surname as surname1', 'miasta.*', 'offeraddres.*', 'offer.id as id1','woje.nazwa as woje','miasta.nazwa as miasto')
+            ->select('offer.*', 'specializations.name as specializations', 'users.name as name1', 'users.surname as surname1', 'miasta.*', 'offeraddres.*', 'offer.id as id1', 'users.id as iksde','woje.nazwa as woje','miasta.nazwa as miasto')
             ->where('offer.status',0)
             ->where('specializations_id',$specializations)
             ->where('miasto_id',$city)
